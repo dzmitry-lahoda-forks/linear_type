96ba6aa6b2ae4576de885756da1792626e0ed82b
Christian Thäter <ct@pipapo.org>
2024-07-15 07:58:59 +0200

    rewrite: NoDrop marker, simpler Linear<T>, more Docs

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # About
 
-This crate defines a `Linear<T>` type. This is a type whose values can not be dropped and must
-be eventually consumed with the `into_inner()` method. Failing to do so will result in a panic
-or compile error.
+This crate strives to implement [linear
+types](https://en.wikipedia.org/wiki/Substructural_type_system#Linear_type_systems).
+
+* The [`Linear<T>`] type that wraps a `T`. Linear types must not be dropped but eventualy consumed.
+  There are only 3 methods you can use on a linear type:
+  1. [`Linear::new()`] creates a new linear type. Note the
+  2. [`Linear::into_inner()`] destructures a object returning the inner value as non
+     linear type.
+  3. [`Linear::map()`] applies a `FnOnce` with the destructured inner type as parameter
+     yielding another linear type.
 
 Unlike `Pin`, linear types can be moved, and unlike `ManuallyDrop`, linear types are required to be
-eventually deconstructed unless they are forgotten with `mem::forget()`. 
+eventually deconstructed and consumed.
 
 
 ## Status
@@ -21,9 +28,45 @@ be somewhat in flux before a 1.0 version is released.
 * **`drop_unchecked`**  
 
   When this crate is compiled with the `drop_unchecked` feature flag, then, in release builds,
-  the contained value is simply wrapped in a `ManuallyDrop` and the check if a value is
-  consumed with `.into_inner()` is omitted. While this is still safe, the linear-type semantic
-  is not enforced and programs may leak resources for objects that are not properly consumed
-  with `.into_inner()`. This defeats the purpose of this crate and adds only a small space and
-  performance improvement. It should only be enabled on programs that are thoroughly validated
-  and tested when required.
+  dropping a linear type will not panic as intended. The linear-type semantic is not
+  enforced. This defeats the purpose of this crate and adds only a small space and performance
+  improvement. It should only be enabled on programs that are thoroughly validated and tested
+  when required.
+
+
+# Example
+
+While any type can be wraped in a `Linear<T>`, it is recommended to use it with unique newtypes
+which transitioning into a final state. The state trasitions can be functions or closures.
+
+```rust
+use linear_type::Linear;
+use std::fs::File;
+use std::io::Read;
+
+// define some newtypes for the states
+struct Filename(String);
+struct ReadonlyFile(File);
+struct FileContent(String);
+
+// define functions that transitions from one state to the next
+fn open_file(Filename(name): Filename) -> ReadonlyFile {
+    ReadonlyFile(File::open(name).unwrap())
+}
+
+fn read_text(ReadonlyFile(mut file): ReadonlyFile) -> FileContent {
+    let mut text = String::new();
+    file.read_to_string(&mut text).unwrap();
+    FileContent(text)
+}
+
+// Create a linear type and transition through the states
+let file_content = Linear::new(Filename("README.md".to_string()))
+    .map(open_file)
+    .map(read_text);
+
+// destructure the file content
+let FileContent(text) = file_content.into_inner();
+assert!(text.contains("# Example"));
+```
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,111 +1,74 @@
 #![doc = include_str!("../README.md")]
 
-use std::ops::Deref;
-
-#[allow(unused_imports)]
-use std::mem::ManuallyDrop;
-
-/// A Cell like struct that wraps a T and can be derefernced to &T.  This cell can not be
-/// dropped. For destruction of the inner value one has to destructure the linear type with
-/// `.into_inner()`. Usually this is done in manual destructors.
-#[cfg(any(debug_assertions, not(feature = "drop_unchecked")))]
+/// A linear type that must be destructured to access the inner value.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Linear<T>(Option<T>);
+#[must_use]
+pub struct Linear<T>(T, NoDrop);
 
-#[doc(hidden)]
-#[cfg(all(not(debug_assertions), feature = "drop_unchecked"))]
+impl<T> Linear<T> {
+    /// Creates a new linear type.
+    pub const fn new(inner: T) -> Self {
+        Self(inner, NoDrop)
+    }
+
+    /// Destructures the linear type and returns the inner type.  This must eventually be called on
+    /// any linear type, failing to do so will panic.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use linear_type::Linear;
+    /// let linear = Linear::new(123);
+    /// let inner = linear.into_inner();
+    /// assert_eq!(inner, 123);
+    /// ```
+    pub fn into_inner(self) -> T {
+        let Linear(t, n) = self;
+        std::mem::forget(n);
+        t
+    }
+
+    /// Transforms one linear type to another linear type. The inner value is passed to the
+    /// closure and the return value is wrapped in a `Linear`.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use linear_type::Linear;
+    /// let number = Linear::new(123);
+    /// let string = number.map(|x| x.to_string());
+    /// assert_eq!(string.into_inner(), "123");
+    /// ```
+    pub fn map<F: FnOnce(T) -> R, R>(self, f: F) -> Linear<R> {
+        Linear::new(f(self.into_inner()))
+    }
+}
+
+/// A marker type that can not be dropped.
+///
+/// # Panics
+///
+/// When the `drop_unchecked` feature is not enabled, this type will panic when dropped.
+/// This is to ensure that linear types are not dropped and must be destructured manually.
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Linear<T>(ManuallyDrop<T>);
-
-#[cfg(any(debug_assertions, not(feature = "drop_unchecked")))]
-impl<T> Linear<T> {
-    /// Creates a new `Linear<T>` wrapping the supplied value.
-    #[must_use]
-    pub fn new(t: T) -> Self {
-        Linear(Some(t))
-    }
-
-    /// Destructures a `Linear<T>` and returns the contained value.  This must eventually be
-    /// called on any `Linear<T>`, failing to do so will panic or show a compile error.
-    #[must_use]
-    pub fn into_inner(mut self) -> T {
-        unsafe {
-            // SAFETY: A program will never see a `Linear<T>` that contains None because only
-            // '.into_inner()' set it to 'None' while consuming 'self'.
-            self.0.take().unwrap_unchecked()
-        }
-    }
-
-    #[inline]
-    fn get(&self) -> &T {
-        unsafe {
-            // SAFETY: A program will never see a `Linear<T>` that contains None because only
-            // '.into_inner()' set it to 'None' while consuming 'self'.
-            self.0.as_ref().unwrap_unchecked()
-        }
-    }
-}
-
-#[cfg(all(not(debug_assertions), feature = "drop_unchecked"))]
-impl<T> Linear<T> {
-    /// Creates a new `Linear<T>` wrapping the supplied value.
-    #[must_use]
-    pub fn new(t: T) -> Self {
-        Linear(ManuallyDrop::new(t))
-    }
-
-    /// Destructures a `Linear<T>` and returns the contained value.  This must eventually be
-    /// called on any `Linear<T>`, failing to do so will panic or show a compile error.
-    #[must_use]
-    pub fn into_inner(mut self) -> T {
-        unsafe {
-            // SAFETY: A program will never see a `Linear<T>` that contains invalid data
-            // because '.into_inner()' consuming 'self'.
-            ManuallyDrop::take(&mut self.0)
-        }
-    }
-
-    #[inline]
-    fn get(&self) -> &T {
-        &*self.0
-    }
-}
-
-impl<T> Deref for Linear<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        self.get()
-    }
-}
-
-impl<T: AsRef<U>, U> AsRef<U> for Linear<T> {
-    fn as_ref(&self) -> &U {
-        self.get().as_ref()
-    }
-}
+#[must_use]
+struct NoDrop;
 
 /// Drop is only implemented when either `debug_assertions` are enabled or the
 /// `drop_unchecked` feature is not enabled.
 #[cfg(any(debug_assertions, not(feature = "drop_unchecked")))]
-impl<T> Drop for Linear<T> {
+impl Drop for NoDrop {
     fn drop(&mut self) {
         // Avoid double panic when we are already panicking
-        assert!(
-            !std::thread::panicking() && self.0.is_none(),
-            "linear type dropped"
-        );
+        #[allow(clippy::manual_assert)]
+        if !std::thread::panicking() {
+            panic!("linear type dropped");
+        }
     }
 }
 
 #[test]
-fn test_destructure() {
-    let linear = Linear::new(123);
-    let _ = linear.into_inner();
-}
-
-#[test]
-#[cfg(all(debug_assertions, feature = "drop_unchecked"))]
+#[cfg(any(debug_assertions, not(feature = "drop_unchecked")))]
 #[should_panic(expected = "linear type dropped")]
 fn test_failed_destructure() {
     let _linear = Linear::new(123);
